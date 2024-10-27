f5e573a89690c4ead22ad2f7e9288c7fd16a9a54
Simon Fritz <simonfr09@gmail.com>
2024-10-27 21:33:17 +0100

    finished name.ts

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -5,37 +5,80 @@ export class Name {
   private components: string[] = [];
   private delimiter: string = this.DEFAULT_DELIMITER;
 
+  /** @methodtype initialization-method */
   constructor(other: string[], delimiter?: string) {
-    throw new Error("needs implementation");
+    if (delimiter) {
+      this.delimiter = delimiter;
+    }
+    this.components = other
   }
 
-    /** Returns human-readable representation of Name instance */
-    public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+  /** Returns human-readable representation of Name instance */
+  /** @methodtype conversion-method */
+  public asNameString(delimiter: string = this.delimiter): string {
+    let nameString: string = ""
+    for (let i = 0; i < this.components.length; i++) {
+      let currentComponent: string = this.components[i]
+      for (let j = 0; j < currentComponent.length; j++) {
+        //add escape character befor delimiter
+        if (currentComponent[j] == this.delimiter) {
+          nameString += this.ESCAPE_CHARACTER + currentComponent[j]
+        }
+        else {
+          nameString += currentComponent[j]
+        }
+      }
+      //adds a delimiter if its not the last component
+      if (i < this.components.length - 1) {
+        nameString += delimiter
+      }
     }
+    return nameString
+  }
 
-    public getComponent(i: number): string {
-        throw new Error("needs implementation");
+  /** @methodtype get-method */
+  public getComponent(i: number): string {
+    if (this.components && i >= 0 && i < this.components.length) {
+      return this.components[i]
     }
+    throw new Error("invalid component");
+  }
 
+  /** @methodtype set-method */
   public setComponent(i: number, c: string): void {
-    throw new Error("needs implementation");
+    if (i >= 0 && i < this.components.length) {
+      this.components[i] = c
+      return
+    }
+    throw new Error("invalid i");
   }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation");
-    }
+  /** Returns number of components in Name instance */
+  /** @methodtype get-method */
+  public getNoComponents(): number {
+    return this.components.length
+  }
 
-    public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+  /** @methodtype command-method */
+  public insert(i: number, c: string): void {
+    if (i >= 0 && i < this.components.length) {
+      this.components.splice(i, 0, c)
+      return
     }
+    throw new Error("invalid i");
+  }
 
+  /** @methodtype command-method */
   public append(c: string): void {
-    throw new Error("needs implementation");
+    this.components.push(c)
   }
 
+  /** @methodtype command-method */
   public remove(i: number): void {
-    throw new Error("needs implementation");
+    if (i >= 0 && i < this.components.length) {
+      this.components.splice(i, 1)
+      return
+    }
+    throw new Error("invalid i");
   }
 }
