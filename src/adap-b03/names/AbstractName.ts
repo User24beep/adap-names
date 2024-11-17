94c479c0b89cfc1ef9e0193d53998b729d2f5e1b
Simon Fritz <simonfr09@gmail.com>
2024-11-17 23:54:25 +0100

    Updated isEqual

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -1,111 +1,98 @@
 import { Name, DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "./Name";
 
 export abstract class AbstractName implements Name {
+  protected delimiter: string = DEFAULT_DELIMITER;
 
-    protected delimiter: string = DEFAULT_DELIMITER;
+  constructor(delimiter: string = DEFAULT_DELIMITER) {
+    this.delimiter = delimiter;
+  }
 
-    constructor(delimiter: string = DEFAULT_DELIMITER) {
-        this.delimiter = delimiter;
+  public asString(delimiter: string = this.delimiter): string {
+    let components: Array<string> = [];
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      components.push(this.unescapeComponent(this.getComponent(i)));
     }
+    return components.join(delimiter);
+  }
 
-    public asString(delimiter: string = this.delimiter): string {
-        let components: Array<string> = []
-        for (let i = 0; i < this.getNoComponents(); i++) {
-            components.push(this.unescapeComponent(this.getComponent(i)));
-        }
-        return components.join(this.getDelimiterCharacter());
+  public toString(): string {
+    return this.asDataString();
+  }
+
+  public asDataString(): string {
+    let components: Array<string> = [];
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      components.push(this.getComponent(i));
     }
+    return components.join(this.getDelimiterCharacter());
+  }
 
-    public toString(): string {
-        return this.asDataString();
+  public isEqual(other: Name): boolean {
+    return this.getHashCode() == other.getHashCode();
+  }
+
+  public getHashCode(): number {
+    let hashCode: number = 0;
+    const s: string = this.asDataString();
+    for (let i = 0; i < s.length; i++) {
+      let c = s.charCodeAt(i);
+      hashCode = (hashCode << 5) - hashCode + c;
+      hashCode |= 0;
     }
+    return hashCode;
+  }
 
-    public asDataString(): string {
-        let components: Array<string> = []
-        for (let i = 0; i < this.getNoComponents(); i++) {
-            components.push(this.getComponent(i));
-        }
-        return components.join(this.getDelimiterCharacter());
+  public clone(): Name {
+    let cloneInstance = Object.create(this.constructor.prototype);
+    cloneInstance.delimiter = this.delimiter;
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      cloneInstance.append(this.getComponent(i));
     }
+    return cloneInstance;
+  }
 
-    public isEqual(other: Name): boolean {
-        if (this.getDelimiterCharacter() != other.getDelimiterCharacter()) {
-            return false;
-        }
-        if (this.getNoComponents() != other.getNoComponents()) {
-            return false;
-        }
-        for (let i = 0; i < this.getNoComponents(); i++) {
-            if (this.getComponent(i) != other.getComponent(i)) {
-                return false;
-            }
-        }
-        return true;
+  public isEmpty(): boolean {
+    return this.getNoComponents() == 0;
+  }
+
+  public getDelimiterCharacter(): string {
+    return this.delimiter;
+  }
+
+  abstract getNoComponents(): number;
+
+  abstract getComponent(i: number): string;
+  abstract setComponent(i: number, c: string): void;
+
+  abstract insert(i: number, c: string): void;
+  abstract append(c: string): void;
+  abstract remove(i: number): void;
+
+  public concat(other: Name): void {
+    if (this.getDelimiterCharacter() != other.getDelimiterCharacter()) {
+      throw new Error("Delimiters have to match for concat to be possible.");
     }
-
-    public getHashCode(): number {
-        let hashCode: number = 0;
-        const s: string = this.asDataString();
-        for (let i = 0; i < s.length; i++) {
-            let c = s.charCodeAt(i);
-            hashCode = (hashCode << 5) - hashCode + c;
-            hashCode |= 0;
-        }
-        return hashCode;
+    for (let i = 0; i < other.getNoComponents(); i++) {
+      this.append(other.getComponent(i));
     }
+  }
 
-    public clone(): Name {
-        let cloneInstance = Object.create(this.constructor.prototype);
-        cloneInstance.delimiter = this.delimiter;
-        for (let i = 0; i < this.getNoComponents(); i++) {
-            cloneInstance.append(this.getComponent(i));
-        }
-        return cloneInstance;
+  //helper-function for asString
+  private unescapeComponent(component: string): string {
+    let unescapedComponent = "";
+    let isEscaping = false;
+    for (let i = 0; i < component.length; i++) {
+      let char = component[i];
+
+      if (isEscaping) {
+        unescapedComponent += char;
+        isEscaping = false;
+      } else if (char === ESCAPE_CHARACTER) {
+        isEscaping = true;
+      } else {
+        unescapedComponent += char;
+      }
     }
-
-    public isEmpty(): boolean {
-        return this.getNoComponents() == 0;
-    }
-
-    public getDelimiterCharacter(): string {
-        return this.delimiter;
-    }
-
-    abstract getNoComponents(): number;
-
-    abstract getComponent(i: number): string;
-    abstract setComponent(i: number, c: string): void;
-
-    abstract insert(i: number, c: string): void;
-    abstract append(c: string): void;
-    abstract remove(i: number): void;
-
-    public concat(other: Name): void {
-        if (this.getDelimiterCharacter() != other.getDelimiterCharacter()) {
-            throw new Error("Delimiters have to match for concat to be possible.");
-        }
-        for (let i = 0; i < other.getNoComponents(); i++) {
-            this.append(other.getComponent(i));
-        }
-    }
-
-    //helper-function for asString
-    private unescapeComponent(component: string): string {
-        let unescapedComponent = "";
-        let isEscaping = false;
-        for (let i = 0; i < component.length; i++) {
-            let char = component[i];
-    
-            if (isEscaping) {
-                unescapedComponent += char;
-                isEscaping = false;
-            } else if (char === ESCAPE_CHARACTER) {
-                isEscaping = true;
-            } else {
-                unescapedComponent += char;
-            }
-        }
-        return unescapedComponent;
-    }
-
+    return unescapedComponent;
+  }
 }
