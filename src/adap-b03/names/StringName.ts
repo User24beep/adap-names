913c89cd4824e271cd5d77ccb64a899e129b718c
Simon Fritz <simonfr09@gmail.com>
2024-11-18 00:06:25 +0100

    fixed a testcase and fixed splitEscaped

--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -2,106 +2,106 @@ import { Name, DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "./Name";
 import { AbstractName } from "./AbstractName";
 
 export class StringName extends AbstractName {
+  protected name: string = "";
+  protected length: number = 0;
 
-    protected name: string = "";
-    protected length: number = 0;
-
-    constructor(other: string, delimiter?: string) {
-        super();
-        if (delimiter) {
-            this.delimiter = delimiter;
-        }
-        this.name = other;
-        let escaped: boolean = false;
-        this.length = 1;
-        for (let i = 0; i < other.length; i++) {
-            //adds +1 to length if a delimiter is found that is not escaped
-            // . -> +1
-            // \. -> +0
-            // \\. -> +1
-            // \\\. -> +0
-            // \\\\. -> +1
-            if (escaped == false && other.charAt(i) == ESCAPE_CHARACTER) {
-                escaped = true;
-                continue;
-            }
-            if (escaped == false && other.charAt(i) == this.delimiter) {
-                this.length += 1;
-                continue;
-            }
-            escaped = false;
-        }
+  constructor(other: string, delimiter?: string) {
+    super();
+    if (delimiter) {
+      this.delimiter = delimiter;
     }
-
-    getNoComponents(): number {
-        return this.length;
-    }
-
-    getComponent(i: number): string {
-        const components = this.splitEscaped(this.delimiter);
-        if (i < 0 || i >= components.length) {
-            throw new Error("Invalid i");
-        }
-        return components[i];
-    }
-
-    setComponent(i: number, c: string) {
-        const components = this.splitEscaped(this.delimiter);
-        if (i < 0 || i >= components.length) {
-            throw new Error("Invalid i");
-        }
-        components[i] = c;
-        this.name = components.join(this.delimiter);;
-    }
-
-    insert(i: number, c: string) {
-        const components = this.splitEscaped(this.delimiter);
-        if (i < 0 || i > components.length) {
-            throw new Error("Invalid i");
-        }
-        components.splice(i, 0, c);
-        this.name = components.join(this.delimiter);
+    this.name = other;
+    let escaped: boolean = false;
+    this.length = 1;
+    for (let i = 0; i < other.length; i++) {
+      //adds +1 to length if a delimiter is found that is not escaped
+      // . -> +1
+      // \. -> +0
+      // \\. -> +1
+      // \\\. -> +0
+      // \\\\. -> +1
+      if (escaped == false && other.charAt(i) == ESCAPE_CHARACTER) {
+        escaped = true;
+        continue;
+      }
+      if (escaped == false && other.charAt(i) == this.delimiter) {
         this.length += 1;
+        continue;
+      }
+      escaped = false;
     }
+  }
 
-    append(c: string) {
-        const components = this.splitEscaped(this.delimiter);
-        components.push(c);
-        this.name = components.join(this.delimiter);
-        this.length += 1;
+  getNoComponents(): number {
+    return this.length;
+  }
+
+  getComponent(i: number): string {
+    const components = this.splitEscaped(this.delimiter);
+    if (i < 0 || i >= components.length) {
+      throw new Error("Invalid i");
     }
+    return components[i];
+  }
 
-    remove(i: number) {
-        const components = this.splitEscaped(this.delimiter);
-        if (i < 0 || i >= components.length) {
-            throw new Error("Invalid i");
-        }
-        components.splice(i, 1);
-        this.name = components.join(this.delimiter);
-        this.length -= 1;
+  setComponent(i: number, c: string) {
+    const components = this.splitEscaped(this.delimiter);
+    if (i < 0 || i >= components.length) {
+      throw new Error("Invalid i");
     }
+    components[i] = c;
+    this.name = components.join(this.delimiter);
+  }
 
-    //helper function that split 
-    private splitEscaped(delimiter: string): string[] {
-        const components: string[] = [];
-        let current = "";
-        let escaped = false;
+  insert(i: number, c: string) {
+    const components = this.splitEscaped(this.delimiter);
+    if (i < 0 || i > components.length) {
+      throw new Error("Invalid i");
+    }
+    components.splice(i, 0, c);
+    this.name = components.join(this.delimiter);
+    this.length += 1;
+  }
 
-        for (let i = 0; i < this.name.length; i++) {
-            const char = this.name.charAt(i);
-            if (escaped) {
-                current += char;
-                escaped = false;
-            } else if (char === ESCAPE_CHARACTER) {
-                escaped = true;
-            } else if (char === delimiter) {
-                components.push(current);
-                current = "";
-            } else {
-                current += char;
-            }
-        }
+  append(c: string) {
+    const components = this.splitEscaped(this.delimiter);
+    components.push(c);
+    this.name = components.join(this.delimiter);
+    this.length += 1;
+  }
+
+  remove(i: number) {
+    const components = this.splitEscaped(this.delimiter);
+    if (i < 0 || i >= components.length) {
+      throw new Error("Invalid i");
+    }
+    components.splice(i, 1);
+    this.name = components.join(this.delimiter);
+    this.length -= 1;
+  }
+
+  //helper function that split
+  private splitEscaped(delimiter: string): string[] {
+    const components: string[] = [];
+    let current = "";
+    let escaped = false;
+
+    for (let i = 0; i < this.name.length; i++) {
+      const char = this.name.charAt(i);
+      if (escaped) {
+        current += char;
+        escaped = false;
+      } else if (char === ESCAPE_CHARACTER) {
+        escaped = true;
+        current += char;
+      } else if (char === delimiter) {
         components.push(current);
-        return components;
+        current = "";
+      } else {
+        current += char;
+      }
     }
+    components.push(current);
+    return components;
+  }
 }
